6e1810d66acebea69a66a66a08703a2e56dc2f2c
J-Man111 <periwinklecloud@hotmail.com>
2026-06-30 09:33:45 -0600

    Add widgets

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                 help="Select the number of forecasted days")
-option = st.selectbox("Sesect data to view",
+option = st.selectbox("Select data to view",
                       ("Tempurature", "Sky",))
 st.subheader(f"{option} for the next {days} days in {place}")
