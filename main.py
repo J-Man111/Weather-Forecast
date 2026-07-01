c632718752c1e873eabe790491d44ba08fecde1b
J-Man111 <periwinklecloud@hotmail.com>
2026-07-01 15:46:36 -0600

    Publish branch

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ option = st.selectbox("Select data to view",
                       ("Tempurature", "Sky",))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+#Get the temperature/sky data
 if place:
-    #Get the temperature/sky data
     filtered_data = get_data(place, days)
 
     if option == "Tempurature":
