89f946978e6629539aa669b628d342fb9d768a97
J-Man111 <periwinklecloud@hotmail.com>
2026-07-01 14:39:39 -0600

    add interactive Sky condition forecast

--- backend.py
+++ backend.py
@@ -1,21 +1,17 @@
 import requests
 
 
-API_KEY = "dc875a2e4f8207264f8d749b726b4ab2"
+API_KEY = "c91fafc9ff578769af657983cf0b9b2b"
 
 
-def get_data(place, forecast_days=None, kind=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=imperial"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ =="__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
