75a0ba57d3d70bda4a7d8d1b16850d82aa078fdd
J-Man111 <periwinklecloud@hotmail.com>
2026-07-03 16:49:15 -0600

    Get rid of error message when city selection box has an invalid city

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
+import streamlit as st
 import requests
-
+import pytz
+from datetime import datetime
 
 API_KEY = "c91fafc9ff578769af657983cf0b9b2b"
 
@@ -8,10 +10,18 @@ def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=imperial"
     response = requests.get(url)
     data = response.json()
-    filtered_data = data["list"]
+    filtered_data = []
+    try:
+        filtered_data = data["list"]
+    except KeyError:
+        st.subheader("Please enter a valid city.")
+        
     nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    return filtered_data
-
+    if len(filtered_data) != 0:
+        filtered_data = filtered_data[:nr_values]
+        return filtered_data     
+    else:
+        return []
+    
 if __name__ =="__main__":
     print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -15,18 +15,20 @@ st.subheader(f"{option} for the next {days} days in {place}")
 if place:
     filtered_data = get_data(place, days)
 
-    if option == "Tempurature":
-        #Create a temperature plot
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures,
-                        labels={"x":"Date", "y": "Temperature (F)"})
-        st.plotly_chart(figure)
+    if len(filtered_data) > 0:
 
-    if option == "Sky":
-        images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png", "Rain":"images/rain.png", "Snow":"images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"]
-                        for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        print(sky_conditions)
-        st.image(image_paths, width=115)
+        if option == "Tempurature":
+            #Create a temperature plot
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures,
+                            labels={"x":"Date", "y": "Temperature (F)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png", "Rain":"images/rain.png", "Snow":"images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"]
+                            for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=115)
